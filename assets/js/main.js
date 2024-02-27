0a8212e2fad603aa51fea8a724817429e8c2591a
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-02-27 15:33:48 +0100

    Palindroma Done - Pari e Dispari Started

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 console.log("Ben collegato");
 
 //Creo una variabile e le assegno come valore una stringa
-const myWord = "annnnnna";
+/*const myWord = "annnnnna";
 const myWord2 = "giacomo";
 
 //Provo a muovermi dentro la stringa come se fosse un array
 //console.log(myWord[2]); // Funziona
 
 //Creo un ciclio for dove utilizzo due indici per l'array
-/*for (let index = 0; index < myWord.length; index++) {
+for (let index = 0; index < myWord.length; index++) {
     //Uno parte dall'inizio
     const firstLetter = myWord[index];
     //Verifico la giusta selezione
@@ -34,6 +34,39 @@ const myWord2 = "giacomo";
 // il codice in una funzione per essere riutilizza
 
 //Cerco un nome adatto alla funzione
-function isThisWordPalindrome(params) {
-    
+//Il parametro myWord è diverso dalla variabile omonima
+function isThisWordPalindrome(myWord) {
+    //Assegno il valore in entrata a una variabile interna
+    checkedWord = myWord;
+    //Copio il codice scritto sopra (ora commentato)
+    for (let index = 0; index < myWord.length; index++) {
+        //Uno parte dall'inizio
+        const firstLetter = myWord[index];
+        //Verifico la giusta selezione
+        //console.log(firstLetter);
+        //L'altro parte dalla fine e avanza in modo speculare
+        const lastLetter = myWord[myWord.length - (index + 1)]
+        //console.log(lastLetter);
+        // Se sono diversi interrompo il ciclo se no il ciclo continua
+        if (firstLetter !== lastLetter) {
+            //console.log("La parola non è palindroma");
+            //break;
+            return false;
+        } else if ( index===(myWord.length-1)){
+            //Se completo il ciclo senza interruzioni significa che la parola è palindroma
+            return true;
+        }
+        // Entrerò nell'else solo quando le mie variabili si saranno
+        // invertite la posizione (essendo la parola palindroma)
+    } 
 }
+
+//Chiamo la funzione e faccio un test
+//isThisWordPalindrome("pippo")
+//isThisWordPalindrome("anna")
+
+//Faccio la stessa cosa ma con il promt
+const myWord = prompt("Inserisci una parola con lettere minuscole")
+console.log("Hai inserito", myWord);
+//Visualizzo con valori booleani se la parola è palindroma
+console.log( isThisWordPalindrome(myWord));
