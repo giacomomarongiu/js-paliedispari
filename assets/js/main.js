c42bbbf7a22f733c0d175a22e6704031bcb3443e
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-02-27 17:07:48 +0100

    cleaned - push

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,14 @@ for (let index = 0; index < myWord.length; index++) {
 
 //Cerco un nome adatto alla funzione
 //Il parametro myWord è diverso dalla variabile omonima
+
+/**
+ * Questa funzione prende in input una parola. Restiurà un valore booleano True se
+ * palindroma, false in caso contrario.
+ * @param {string} myWord 
+ * @returns {boolean}
+ */
+
 function isThisWordPalindrome(myWord) {
     //Assegno il valore in entrata a una variabile interna
     checkedWord = myWord;
@@ -66,28 +74,25 @@ function isThisWordPalindrome(myWord) {
 //isThisWordPalindrome("anna")
 
 //Faccio la stessa cosa ma con il promt
-//const myWord = prompt("Inserisci una parola con lettere minuscole")
-//console.log("Hai inserito", myWord);
+const myWord = prompt("PRIMO ESERCIZIO Inserisci una parola con lettere minuscole")
+console.log("Hai inserito", myWord);
 //Visualizzo con valori booleani se la parola è palindroma
-//console.log( isThisWordPalindrome(myWord));
+console.log( isThisWordPalindrome(myWord));
 
 
 
 //Ex 2
 //PARI E DISPARI
 //Dichiaro due variabili
-const userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
-const choice = Number(prompt("Inserisci 0 se pensi il numero sia pari, 1 se dispari"))
+const userNumber = Number(prompt("SECONDO ESERCIZIO Inserisci un numero da 1 a 5"));
+console.log("hai inserito il numero", userNumber);
+const choice = Number(prompt("SECONDO ESERCIZIO Inserisci 0 se pensi il numero sia pari, 1 se dispari"))
 console.log("hai inserito", choice);
-let randomNum= Number(Math.floor(Math.random() * 5));
-let sum = randomNum + userNumber;
 
-//Vedo la somma
-console.log("la somma è",sum); //ok2
 
 //Devo fare due verifiche
 //La prima è capire se il numero random è pari/dispari
-if (sum % 2 === 0) {
+/*if (sum % 2 === 0) {
     //test
     console.log("Il numero è pari");
 } else {
@@ -97,5 +102,50 @@ if (sum % 2 === 0) {
 //La seconda è capire se la mia scelta è uguale all'esito del confronto precendente
 if ((sum % 2 === choice)) {
     console.log("hai vinto");
- } else { console.log("hai perso"); }
+} else { console.log("hai perso"); }*/
 
+
+
+//Guardo ciò che ho scritto e cerco di incorporare
+// il codice in una funzione per essere riutilizza
+
+//Cerco un nome adatto alla funzione
+//Ha bisogno di due parametri (in questo caso due numeri)
+
+/**
+ * Questa funzione prende in input dua valori numerici: il primo da 1 a 5 verrà sommato
+ * a una cifra casuale (sempre da 1 a 5). Il secondo è una previsione sulla somma del numero:
+ * 0 se si pensa sarà pari, 1 se si pensa sarà dispari. Restituirà una stringa con 
+ * VITTORIA/SCONFITTA.
+ * @param {number} userNumber 
+ * @param {number} choice 
+ * @returns {string}
+ * */
+
+function numberHeadsOrTails(userNumber, choice) {
+    //Calcolo somma qui all'interno
+    let randomNum = Number(Math.floor(Math.random() * 5));
+    let sum = randomNum + userNumber;
+    //Vedo la somma
+    console.log("la somma è", sum); //ok2
+
+    //Devo fare due verifiche
+    //La prima è capire se il numero random è pari/dispari
+    if (sum % 2 === 0) {
+        //test
+        console.log("Il numero è pari");
+    } else {
+        console.log("E' dispari");
+    }
+
+    //La seconda è capire se la mia scelta è uguale all'esito del confronto precendente
+    if ((sum % 2 === choice)) {
+        return ("hai vinto");
+    } else { return ("hai perso"); }
+}
+
+//Chiamo la funzione con due valori messi a mano
+//console.log(numberHeadsOrTails(5,1));
+
+//Chiamo la funzione con il promt
+console.log(numberHeadsOrTails(userNumber, choice));
